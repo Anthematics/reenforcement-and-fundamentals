2b1f485b8137c23edd29bcf2f35dfa8acb4fb580
Jason Harder <Jasonharder2@gmail.com>
2017-08-22 22:58:25 -0400

    Add files via upload
    v.5 aug 23 reinforcement exercise - has not been tested. - check syntax tomorrow on ubuntu (written atom in windows)

---
+++ aug23reinforce.rb
@@ -0,0 +1,26 @@
+{ data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50},
+      { id: 2, room_number: "301", capacity: 200 },
+       { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+
+
+Retrieve the capacity of room 201 and store it in a variable.
+
+room201cap = rooms[1][:capacity]
+puts room201cap
+
+# Find all the events taking place in room 201. Iterate through them and print "ok" if the number of planned attendees will fit in the room.
+
+room201events = events.each do
+  if :attendees <= 50
+    print "ok"
