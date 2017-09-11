e2aa56d25a679388ed7d3e5a6e0f432d8f9bbc70
Anthematics <jasonharder2@gmail.com>
2017-09-11 14:44:31 -0400

    added aug 28 - still need to work out a bug here and there

---
+++ aug24reinforce.rb
@@ -0,0 +1,18 @@
+class Location
+	attr_accessor :name
+
+	def initialize(name)
+		@name=name
+	end
+
+class Trip
+	@@stops = []
+
+
+ def newlocation(name)
+	 @@stops << Location.new(name)
+ end
+
+ def show_trips
+	 return @@stops
+end
