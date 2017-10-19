94036eddb70667abcb30bc04eb64d103be5f6e59
Anthematics <jasonharder2@gmail.com>
2017-10-19 00:46:23 -0400

    code still buggy

--- oct5reinforce/test_team.rb
+++ oct5reinforce/test_team.rb
@@ -5,10 +5,26 @@ require './team.rb'
 class TestTeam < MiniTest::Test
 
 	def test_to_hash
-		 Team.new(jasonsteam,5,500)
-		 assert (jasonsteam,5,500)
+		 superteam = Team.new('jasonsteam',5,['job','bob'])
+		 assert_equal {(:team_name=>'jasonsteam',:level=>5,:points=>100}, superteam.to_hash
+	end
+
+end
 
 
+	def initialize(name, level, team_member_names)
+		@name = name
+		@level = level
+		@members = team_member_names
+		@points = 0
+	end
+
+	def to_hash
+		hash = {}
+		hash[:team_name] = @name
+		hash[:level] = @level
+		hash[:points] = @points
+		return hash
 	end
 
 end
