7e943f9b01e840cfc4d24e4d1e15f88600130336
Anthematics <jasonharder2@gmail.com>
2017-10-19 00:47:31 -0400

    brackets were reversed :/

--- oct5reinforce/test_team.rb
+++ oct5reinforce/test_team.rb
@@ -4,27 +4,10 @@ require './team.rb'
 
 class TestTeam < MiniTest::Test
 
+
 	def test_to_hash
-		 superteam = Team.new('jasonsteam',5,['job','bob'])
-		 assert_equal {(:team_name=>'jasonsteam',:level=>5,:points=>100}, superteam.to_hash
-	end
-
-end
-
-
-	def initialize(name, level, team_member_names)
-		@name = name
-		@level = level
-		@members = team_member_names
-		@points = 0
-	end
-
-	def to_hash
-		hash = {}
-		hash[:team_name] = @name
-		hash[:level] = @level
-		hash[:points] = @points
-		return hash
+		 superteam = Team.new('jasonsteam',5,100['job','bob'])
+		 assert_equal ({:team_name=>'jasonsteam',:level=>5,:points=>100}), superteam.to_hash
 	end
 
 end
